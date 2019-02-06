4712e4ee150c353d62e27ec8d97962a0756f3a35
Carrie Walsh <carriewalsh@gmail.com>
2019-02-06 09:32:22 -0700

    Adds Building/Test classes

---
+++ test/building_test.rb
@@ -0,0 +1,9 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/apartment"
+require "./lib/renter"
+require "./lib/building"
+require "pry"
+
+class BuildingTest < MiniTest::Test
+end
